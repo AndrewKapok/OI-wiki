0341e614e54a862c0e1fa628145bfe4706d8a9e5
HeRaNO <HeRaNO@users.noreply.github.com>
2026-09-12 03:20:50 +0000

    deploy: c34a2cc9b5749713f30414226ba761d5a387b91c

---
+++ _static/js/oiwiki-feedback-sys-frontend.js
@@ -0,0 +1,32 @@
+sessionStorage.setItem("commitHash", "c34a2cc9b5749713f30414226ba761d5a387b91c"); // commit hash injected here, see: scripts/pre-build/install-feedback-sys-frontend
+
+function matchColor() {
+  const palettle = localStorage.getItem("/.__palette");
+  if (
+    (palettle !== null && JSON.parse(palettle)?.color?.scheme !== "default") ||
+    (palettle === null && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("review_dark_mode");
+  } else {
+    document.documentElement.classList.remove("review_dark_mode");
+  }
+}
+
+function hookMkdocsMaterial() {
+  document.querySelector(".md-header__option").addEventListener("click", e => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+    setTimeout(matchColor, 0); // wait for the theme to be applied
+  });
+}
+
+hookMkdocsMaterial();
+
+document$.subscribe(function () {
+  matchColor();
+
+  globalThis["OIWikiFeedbackSysFrontend"] instanceof Object &&
+    OIWikiFeedbackSysFrontend.setupReview instanceof Function &&
+    OIWikiFeedbackSysFrontend.setupReview(document.body, {
+      apiEndpoint: "https://feedback-sys.mgt.moe/" // api endpoint injected here, see: scripts/pre-build/install-feedback-sys-frontend
+    });
+});
